72069095f708fd85b327e8bd91902500bb2ff3db
SergeantRyan <159430565+SergeantRyan@users.noreply.github.com>
2025-01-08 19:57:18 +0100

    Add files via upload

--- Calendrier.py
+++ Calendrier.py
@@ -1,229 +1,191 @@
 from icalendar import Calendar
 from subprocess import *
-import subprocess
-import sys
-import requests
 from pkg_resources import *
-import pkg_resources
+import collections
 
-FichierL1 = "C:/Users/TideP/Downloads/ADECal_L1INFO.ics"
-FichierL2 = "ADECal_L2INFO.ics"
-FichierL3 = "ADECal_L3INFO.ics"
-Fichier_EL108 = "ADECal_EL108.ics"
+SallesCM={"JJ035","EB020","EB021","JJ030"}
+SallesTD={"EB209","JJ012","EB116","JJ123"}
+SallesSPA={"EL106A","EL106B","EL107","EL108","EL109"}
+SallesTP={"EB110","EB109","MM105","MM001","MM002","MM003","MM004","MM005","MM101","MM102","MM103","MM104","MM105","JJ201","JJ202"}
+SallesPhysique={"EB001","EB001 BIS","EB005","EB018"}
+SallesChimie={"EB013","EB015"}
+SallesBio={"EB011","EB016","EB102","EB107"}
 
-fichier=FichierL1
+JoursFeries={"1111","0105","0805","2905"}
+NombreSemaines=29
+VolumeTotale=NombreSemaines*5*8*60
+JoursFeries=5
 
 def lire(nom_fichier):
     g = open(nom_fichier, 'rb')
     Calendrier = Calendar.from_ical(g.read())
     L = Calendrier.walk()[1:]
-    return parcours(L)
+    return parcours2(L)
 
 def heure(sceance):
     debut=str(sceance.get("DTSTART"))[21:26]
     fin=str(sceance.get("DTEND"))[21:26]
     return debut,fin
 
-def duree(sceance):
+def mins(sceance):
     debut,fin=heure(sceance)
     minutes=int(fin[3:5])-int(debut[3:5])
     heures=int(fin[0:2])-int(debut[0:2])
     if minutes<0:
         minutes+=60
         heures-=1
-    return heures, minutes
+    return minutes+60*heures
 
-def conversion(heures,minutes):
-    while minutes>=60:
-        minutes-=60
-        heures+=1
-    return heures,minutes
+def conversion(minutes):
+    return minutes//60,minutes%60
+
+def tauxOccupation(nH,totale):
+    return nH*100/totale
+
+def TauxOcc(nME,nJours):
+    print(nME,nJours*8*60)
+    print(conversion(nME))
+    return str((nME*100)/(8*nJours*60))
 
 def deconversion(heures,minutes):
-    while heures>0:
-        minutes+=60
-        heures-=1
-    return minutes
+    return minutes+heures*60
 
 def read_desc(sceance):
     prop=sceance.get("DESCRIPTION")
     t=prop.split("\n")
     return t
 
-def proportions(htotale,htp,htd,hcm,totale,tp,td,cm):
-    tp=deconversion(htp,tp)
-    td=deconversion(htd,td)
-    cm=deconversion(hcm,cm)
-    totale=deconversion(htotale,totale)
-    return (round(tp*100/totale),round(td*100/totale),round(cm*100/totale))
+def getSalle(sceance):
+    salle=sceance.get("LOCATION")
+    if salle[0:9]=="EB001 BIS":
+        return "EB001 BIS"
+    elif salle[0:5]=="EL106":
+        return salle[0:6]
+    return salle[0:5]
 
-def totale_minutes(heures):
-    somme_minutes=0
-    for temps in heures:
-        heure=temps[0]
-        minute=temps[1]
-        minutes=deconversion(heure,minute)
-        somme_minutes+=minutes
-    return somme_minutes
+def pourcentages(D):
+    totale=sum(D[i] for i in D)
+    return {i:round(D[i]*100/totale) for i in D}
 
-def proportions_salles(heures): 
-    somme_minutes=totale_minutes(heures)
-    proportions=[]
-    for temps in heures:
-        heure=temps[0]
-        minute=temps[1]
-        minutes=deconversion(heure,minute)
-        proportion=round(minutes*100/somme_minutes)
-        proportions.append(proportion)
+def proportionsSalles(minutes):
+    proportions=dict()
+    somme=sum(minutes[i] for i in minutes)
+    for temps in minutes:
+        proportions[temps]=round(minutes[temps]*100/somme)
     return proportions
 
-def get_prof(sceance):
-    infos=read_desc(sceance)[2:]
+def get_prof(infos):
     for info in infos:
         if not (("TP" in info) or ("TD" in info) or ("CM" in info) or ("Exporté" in info) or (info=="")):
             return info
 
-def parcours(calendrier):
-    heures_TP=0
-    heures_TD=0
-    heures_CM=0
-    minutes_CM=0
-    minutes_TP=0
-    minutes_TD=0
-    somme_heures=0
-    somme_minutes=0
+def groupe1(sceance):
+    info=sceance.get("SUMMARY")
+    if "Groupe-01" in info or "Groupe-A" in info:
+        return 1
+    return 0
+
+def getCode(sceance):
+    s=sceance.get("SUMMARY")
+    ch=""
+    for i in s:
+        if i in {" ",""}:
+            return ch
+        ch+=i
+    return ch
+
+def parcours1(calendrier):
+    repartition={"TP":0,"SPA":0,"TD":0,"CM":0}
     for sceance in calendrier:
         prop=read_desc(sceance)
-        print(prop)
-        duration=duree(sceance)
-        if prop[3][0:2]=="CM":
-            heures_CM+=duration[0]
-            minutes_CM+=duration[1]
-            somme_heures+=duration[0]
-            somme_minutes+=duration[1]
-            '''if minutes_CM>=60:
-                minutes_CM-=60
-                heures_CM+1'''
-        elif prop[3][0:2]=="TD":
-            if "Groupe-01" in prop[3]:
-                heures_TD+=duration[0]
-                minutes_TD+=duration[1]
-                somme_heures+=duration[0]
-                somme_minutes+=duration[1]
-                '''if minutes_TD>=60:
-                    minutes_TD-=60
-                    heures_TD+=1
-                '''
-                
-        elif prop[3][0:2]=="TP":
-            if "Groupe-A" in prop[3]:
-                heures_TP+=duration[0]
-                minutes_TP+=duration[1]
-                somme_heures+=duration[0]
-                somme_minutes+=duration[1]
-                '''if minutes_TP>=60:
-                    minutes_TP-=60
-                    heures_TP+=1
-                    somme_minutes-=60
-                    somme_heures+=1
-                '''
+        minutes=mins(sceance)
+        salle=getSalle(sceance)
+        grp=groupe1(sceance)
+        if salle in SallesSPA:
+            repartition["SPA"]+=minutes
+        elif salle in SallesTP:
+            if grp:
+                repartition["TP"]+=minutes
+        elif salle in SallesCM:
+            repartition["CM"]+=minutes
+        elif salle=="":
+            print(prop)
         else:
-            print(read_desc(sceance),duree(sceance))
-            #print("SKIP LINE")
-        '''
-        if "TD" in prop[3] or "TP" in prop[3] or "CM" in prop[3]:
-            somme_heures+=duration[0]
-            somme_minutes+=duration[1]
-            if somme_minutes>=60:
-                somme_minutes-=60
-                somme_heures+=1
-        '''
-    
-    heures_TD,minutes_TD=conversion(heures_TD,minutes_TD)
-    heures_CM,minutes_CM=conversion(heures_CM,minutes_CM)
-    heures_TP,minutes_TP=conversion(heures_TP,minutes_TP)
-    somme_heures,somme_minutes=conversion(somme_heures,somme_minutes)
-    return [proportions(somme_heures,heures_TP,heures_TD,heures_CM,somme_minutes,minutes_TP,minutes_TD,minutes_CM),[somme_heures,somme_minutes,heures_TP,minutes_TP,heures_TD,minutes_TD,heures_CM,minutes_CM]]
-    #return ((somme_heures,somme_minutes),(heures_TP,minutes_TP),(heures_TD,minutes_TD),(heures_CM,minutes_CM))
-    #return conversion(heures_TP+heures_TD+heures_CM,minutes_CM+minutes_TD+minutes_TP)
+            if grp:
+                repartition["TD"]+=minutes
+    repartition["TD/CM"]=repartition["TD"]+repartition["CM"]
+    return repartition
 
-def parcours_salle(calendrier):
-    matières=[]
-    heures=[]
-    profs=[]
+def parcours2(calendrier):
+    repartition={"INFO":0,"SPA":0,"TD":0,"CM":0,"PHY":0,"BIO":0,"CH":0}
+    for sceance in calendrier:
+        salle=getSalle(sceance)
+        if salle in SallesSPA:
+            grp=groupe1(sceance)
+            if grp:
+                minutes=mins(sceance)
+                repartition["SPA"]+=minutes
+        elif salle in SallesTP:
+            grp=groupe1(sceance)
+            if grp:
+                minutes=mins(sceance)
+                repartition["INFO"]+=minutes
+        elif salle in SallesCM:
+            minutes=mins(sceance)
+            repartition["CM"]+=minutes
+        elif salle in SallesPhysique:
+            grp=groupe1(sceance)
+            if grp:
+                minutes=mins(sceance)
+                repartition["PHY"]+=minutes
+        elif salle in SallesBio:
+            grp=groupe1(sceance)
+            if grp:
+                minutes=mins(sceance)
+                repartition["BIO"]+=minutes
+        elif salle in SallesChimie:
+            grp=groupe1(sceance)
+            if grp:
+                minutes=mins(sceance)
+                repartition["CH"]+=minutes
+        elif salle=="":
+            #prop=read_desc(sceance)
+            #print(prop)
+            pass
+        else:
+            grp=groupe1(sceance)
+            if grp:
+                #print(salle)
+                minutes=mins(sceance)
+                repartition["TD"]+=minutes
+    repartition["TD/CM"]=repartition["TD"]+repartition["CM"]
+    repartition.pop("TD",None)
+    repartition.pop("CM",None)
+    return repartition
+
+def parcoursSalles(calendrier):
+    matieres=dict()
+    minutes=dict()
+    profs=dict()
     for sceance in calendrier:
         infos=read_desc(sceance)
-        matière=infos[1]
-        prof=get_prof(sceance)
-        hduration,mduration=duree(sceance)
-        if not matière in matières:
-            matières.append(matière)
-            heures.append([hduration,mduration])
-            profs.append(prof)
+        matiere=infos[1]
+        code=getCode(sceance)
+        m=mins(sceance)
+        if code in matieres:
+            minutes[matiere]+=m
         else:
-            indice=matières.index(matière)
-            heures[indice][0]+=hduration
-            heures[indice][1]+=mduration
-            heures[indice][0],heures[indice][1]=conversion(heures[indice][0],heures[indice][1])
-    print(heures)
-    return [matières,proportions_salles(heures),profs,heures]
+            minutes[matiere]=m
+            profs[matiere]=infos[-3]
+            matieres[code]=matiere
+    matieres=collections.OrderedDict(sorted(matieres.items()))
+    return matieres,proportionsSalles(minutes),profs,minutes
 
 def lire_salle(nom_fichier):
     g = open(nom_fichier, 'rb')
     Calendrier = Calendar.from_ical(g.read())
     L = Calendrier.walk()[1:]
-    return parcours_salle(L)
+    return parcoursSalles(L)
 
-    '''
-        salle=prop[3]
-        if salle[0:2]=="CM":
-            heures_CM+=duree(prop)[0]
-            minutes_CM+=duree(prop)[1]
-            if minutes_CM>60:
-                minutes_CM-=60
-                heures_CM+=1
-        elif salle[0:2]=="TD":
-            if salle[-1]=="2":
-                somme_heures+=duree(sceance)[0]
-                somme_minutes+=duree(sceance)[1]
-                while somme_minutes>60:
-                    somme_minutes-=60
-                    somme_heures+=1
-    return somme_heures,somme_minutes
-    '''
-
-def install(package):
-    subprocess.check_call([sys.executable, "-m", "pip", "install", package])
-
-'''
-def installer(required):
-    installed = {pkg.key for pkg in pkg_resources.working_set}
-    missing   = required - installed
-
-    if missing:
-    # implement pip as a subprocess:
-        subprocess.check_call([sys.executable, '-m', 'pip', 'install', *missing])
-        
-def run_cmd(cmd):
-    ps = run(cmd, stdout=PIPE, stderr=STDOUT, shell=True, text=True)
-    print(ps.stdout)
-'''
-
-'''
-def read(nom_fichier):
-    L=lire(FichierL1)
-    return parcours(L)
-'''
-
-liste=lire(FichierL1)
-
-'''print(liste)
-for sceance in liste:
-    print(read_desc(sceance))
-    print("")'''
-
-
-'''
-liste=lire_salle(Fichier_EL108)
-print(liste)
-'''
+t=lire_salle("C:/Users/TideP/Downloads/ADECal_SalleEL107_semaine9-12.ics")
+print(t)
